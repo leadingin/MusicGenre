be1338bd8d833cf59876d6f676fa15a0ee635c77
GuangxiaoSong <sgx1988125@hotmail.com>
2016-11-27 21:08:39 +0800

    \x00  exception commit from song

--- data/load_raw_data_file_to_array.py
+++ data/load_raw_data_file_to_array.py
@@ -7,6 +7,7 @@
 """
 
 import numpy as np
+import re
 
 
 class LoadRawDataFileToArray:
@@ -16,7 +17,23 @@ class LoadRawDataFileToArray:
 
     def load(self, path):
         f = open(path, "r")
-        result = [[np.float32(v) for v in line.strip().split(' ')] for line in f]
+        counter = 0
+        result=[[]]
+        for line in f:
+            r = []
+            counter += 1
+            if counter % 50 == 0:
+                print "line %i finished." % (counter)
+            for v in line.strip().split(' '):
+                try:
+                    r.append(np.float32(v))
+                # 处理类似\x00这种打印不出来的情况
+                except:
+                    temp = re.findall(r'[0-9\.\-]', v)
+                    v = ''.join(temp)
+                    r.append(np.float32(v))
+            result.append(r)
         f.close()
+        del result[0]
         print "data shape: %i,%i" %(len(result), len(result[0]))
         return result
