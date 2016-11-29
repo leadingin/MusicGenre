6253e98b5e35255bbde4865733531b15e45f7313
SaerdnaPp <saerdnapp@gmail.com>
2016-11-29 20:31:51 +0800

    tf input csv

--- 0001_merge_file_line_num.py
+++ 0001_merge_file_line_num.py
@@ -1,7 +1,11 @@
 # -*- coding:utf-8 -*-
 
-f = open("data/merge/allRawData.txt")
+f = open("data/merge/allRawData.txt", "r")
 line_num = 0
+line = f.readline()
+str = line.split(" ")
+print len(str)
+print str[-1]
 for line in f:
 	line_num += 1
 print "File has %i lines" % (line_num)
---
+++ data/add_class_in_each_row.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: Songgx
+@file: add_class_in_each_row.py
+@time: 11/28/16 7:20 PM
+"""
+
+TOTAL_ROW_NUM = 1000
+
+# line 0-999
+# line 0-99    class 0
+# line 100-199 class 1
+# ...
+# line 900-999 class 9
+
+class_num = 0
+line_num = 0
+
+fr = open("merge/allRawData.txt", "r")
+fw = open("merge/raw_data.txt", "w")
+for line in fr:
+    # line += str(class_num) not worked
+    line = line.strip() + " " + str(class_num) + "\n" # cancel "\n" and "\r"
+    fw.write(line)
+    line_num += 1
+    if line_num % 100 == 0:
+        class_num += 1
+        print "%i / 1000 lines finished." % (line_num)
+fr.close()
+fw.close()
+
+print "Verify new file:"
+fr1 = open("merge/raw_data.txt", "r")
+for i in range(1000):
+    line_num1 = i + 1
+    l = fr1.readline()[-10:]
+    if (line_num1+1) % 100 == 0 or (line_num1-1) % 100 == 0 or line_num1 % 100 == 0:
+        print "line-" + str(line_num1) + ":" + l.strip()
+fr1.close()
+print "Finished."
+
+
+
+
+
+
+
+
+
+
