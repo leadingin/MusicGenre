e2cae1ce24f4d987b6075243d7a6c35f8c94a32f
GuangxiaoSong <sgx1988125@hotmail.com>
2016-12-10 23:05:43 +0800

    h1:512 h2:512, acc 0.4 1024 1024  0.45-0.6

--- 0504_tf_full_connect_NN.py
+++ 0504_tf_full_connect_NN.py
@@ -7,16 +7,17 @@
 """
 
 from __future__ import print_function
-import tensorflow as tf
+
 import numpy as np
+import tensorflow as tf
 
-tf.logging.set_verbosity(tf.logging.INFO)
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+def csv_file_line_number(fname):
+    with open(fname, "r") as f:
+        num = 0
+        for line in f:
+            num += 1
+    return num
 
 
 def count_column_num(fname, field_delim):
@@ -57,16 +58,23 @@ def read_and_decode(filename):
 # Parameters
 learning_rate = 0.001
 training_epochs = 10000
-display_step = 1
-num_threads = 4
-csv_file_path = "data/tvtsets/test_scat_data.txt"
-training_file_path = "data/tvtsets/test_scat_data.tfrecords"
+display_step = 50
+num_threads = 8
+
+training_csv_file_path = "data/tvtsets/training_scat_data.txt"
+training_file_path = "data/tvtsets/training_scat_data.tfrecords"
+test_csv_file_path = "data/tvtsets/test_scat_data.txt"
+test_file_path = "data/tvtsets/test_scat_data.tfrecords"
 batch_size = 20
-column_num = count_column_num(csv_file_path, " ")
+column_num = count_column_num(training_csv_file_path, " ")
 # file_length = file_len(csv_file_path)
 # Network Parameters
 n_hidden_1 = 1024  # 1st layer number of features
 n_hidden_2 = 1024  # 2nd layer number of features
+
+# h1:512 h2:512, acc is about:0.4
+
+
 n_input = column_num - 1
 n_classes = 10  # total classes (0-9 digits)
 
@@ -75,14 +83,12 @@ n_classes = 10  # total classes (0-9 digits)
 x = tf.placeholder(tf.float32, [batch_size, n_input])
 y = tf.placeholder(tf.int32, [batch_size,])
 
-
-
 # Create model
 def multilayer_perceptron(x, weights, biases):
-    # Hidden layer with RELU activation
+    # Hidden layer with sigmoid activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.sigmoid(layer_1)
-    # Hidden layer with RELU activation
+    # Hidden layer with sigmoid activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.sigmoid(layer_2)
     # Output layer with linear activation
@@ -105,66 +111,84 @@ biases = {
 # Construct model
 pred = multilayer_perceptron(x, weights, biases)
 
-# Define loss and optimizer
+# Define loss and optimizer & correct_prediction
 cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(pred, y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Launch the graph
 
 audio, label = read_and_decode(training_file_path)
+audio_test, label_test = read_and_decode(test_file_path)
+
 
 #使用shuffle_batch可以随机打乱输入
 audio_batch, label_batch = tf.train.shuffle_batch([audio, label],
                                                 batch_size=batch_size, capacity=2000,
                                                 min_after_dequeue=1000)
 
-sess = tf.Session()
+# Initializing the variables
 init = tf.global_variables_initializer()
-sess.run(init)
-tf.train.start_queue_runners(sess=sess)
 
-for i in range(10000):
-    # pass it in through the feed_dict
-    audio_batch_vals, label_batch_vals = sess.run([audio_batch, label_batch])
-
-    _, loss_val = sess.run([optimizer, cost], feed_dict={x:audio_batch_vals, y:label_batch_vals})
-    print (loss_val)
-
-
-
-'''
+# Launch the graph
 with tf.Session() as sess:
     sess.run(init)
 
-    t1=time.time()
-    # Training cycle
-    for epoch in range(training_epochs):
-        # Loop over all data
-        for serialized_example in tf.python_io.tf_record_iterator(training_file_path):
-            # Get serialized example from file
-            example = tf.train.Example()
-            example.ParseFromString(serialized_example)
-            # Read data in specified format
-            label = example.features.feature["label"].int64_list.value
-            features = example.features.feature["features"].float_list.value
-            # solve error: ValueError: Argument must be a dense tensor, use nparray as input
-            features_array = np.array([features])
-            features_array = np.reshape(features_array, (1, n_input))
-            label_array = dense_to_one_hot(np.array([label]), num_classes = n_classes)
+    # Start input enqueue threads.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    for epoch in range(10000):
+        # pass it in through the feed_dict
+        audio_batch_vals, label_batch_vals = sess.run([audio_batch, label_batch])
 
-            _, c = sess.run([optimizer, cost], feed_dict={x: features_array, y: label_array})
-        # Display logs per epoch step
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(c))
-    print("Optimization Finished!")
-    t2 = time.time()
-    print("Training cost: " + str(t2-t1) + " s")
+        _, loss_val = sess.run([optimizer, cost], feed_dict={x:audio_batch_vals, y:label_batch_vals})
+        if (epoch + 1) % display_step == 0:
+            print("Epoch:", '%06d' % (epoch + 1), "cost=", "{:.9f}".format(loss_val))
 
-
+    print("Training finished.")
+    coord.request_stop()
+    coord.join(threads)
 
     # Test model
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     # Calculate accuracy
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
-'''
+    test_example_number = csv_file_line_number(test_csv_file_path)
+    correct_num = 0
+    for _ in range(test_example_number):
+        audio_test_val, label_test_val = sess.run([audio_test, label_test])
+        audio_test_val_vector = np.array([audio_test_val])
+        test_pred = multilayer_perceptron(audio_test_val_vector, weights, biases)
+        '''
+        print (sess.run([test_pred]))
+
+        [array([[ 11.13519478,  12.56501865,  14.68154907,   2.02798128,
+         -5.89219952,  -0.76298785,   0.46614531,   5.27717066,
+          7.54774714,   7.12729597]], dtype=float32)]
+        '''
+        pred_class_index = sess.run(tf.argmax(test_pred, 1))
+        '''
+        print (sess.run(tf.argmax(test_pred, 1)))
+
+        [8]
+        0
+        [5]
+        0
+        [9]
+        0
+        [8]
+        0
+         ....
+
+        [9]
+        9
+        [9]
+        9
+        [4]
+        9
+        [1]
+        9
+        '''
+
+        if (label_test_val == pred_class_index[0]):
+            correct_num += 1
+    print (("%i / %i is correct.") % (correct_num, test_example_number))
+    print (("Accuracy is %f .") % (float(correct_num) / test_example_number))
+    sess.close()
