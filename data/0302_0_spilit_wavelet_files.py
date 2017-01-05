74d77005550c89f4f1080213145acd8d54ab04dd
GuangxiaoSong <sgx1988125@hotmail.com>
2017-01-05 18:21:45 +0800

    convert wavelets to tfrecords

---
+++ data/0302_0_spilit_wavelet_files.py
@@ -0,0 +1,81 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: Songgx
+@file: 0302_0_spilit_wavelet_files.py
+@time: 2016/12/26 22:30
+"""
+
+
+import os
+import numpy as np
+
+# 每一类音乐的原个数，于之前分割时的命名习惯有关
+each_class_source_num = 100
+
+class_name_array = [
+        'blues',
+        'classical',
+        'country',
+        'disco',
+        'hiphop',
+        'jazz',
+        'metal',
+        'pop',
+        'reggae',
+        'rock']
+
+
+def split_training_test(wavelet_path, out_put_path, training_scale):
+    training_set_path = out_put_path + '/training_set.txt'
+    test_set_path = out_put_path+'/test_set.txt'
+
+    # 删除之前的training_set test_set文件
+    if os.path.exists(training_set_path):
+        os.remove(training_set_path)
+    if os.path.exists(test_set_path):
+        os.remove(test_set_path)
+
+    # 创建新的training_set test_set文件并打开
+    f_training = open(training_set_path, 'a')
+    f_test = open(test_set_path, 'a')
+
+    # 类别的名字，每个类别按照比例生成随机数列，将最终的文件名写入文件
+    for class_name in class_name_array:
+        index = np.random.permutation(each_class_source_num)
+        training_index = index[:int(training_scale * index.size)]
+        test_index = index[int(training_scale * index.size):]
+
+        cur_class_training_file_pre_names = []
+        for cur_index in training_index:
+            if cur_index < 10:
+                cur_class_training_file_pre_names.append(class_name + '.0000' + str(cur_index))
+            else:
+                cur_class_training_file_pre_names.append(class_name + '.000' + str(cur_index))
+
+        cur_class_test_file_pre_names = []
+        for cur_index in test_index:
+            if cur_index < 10:
+                cur_class_test_file_pre_names.append(class_name + '.0000' + str(cur_index))
+            else:
+                cur_class_test_file_pre_names.append(class_name + '.000' + str(cur_index))
+        # test file name prefix
+        # for name in cur_class_test_file_pre_names:
+        #    print(name)
+        for _root, _dirs, files in os.walk(wavelet_path):
+            for file in files:
+                for training_prefix in cur_class_training_file_pre_names:
+                    if file.find(training_prefix) != -1:
+                        f_training.write(file + '\n')
+                for test_prefix in cur_class_test_file_pre_names:
+                    if file.find(test_prefix) != -1:
+                        f_test.write(file + '\n')
+
+    f_training.close()
+    f_test.close()
+
+    print("Generating index_file of training & test set finished.")
+
+
+if __name__ == '__main__':
+    split_training_test('C:/Users/song/data/wavelets', 'C:/Users/song/data/wavelets', 0.8)
---
+++ data/0302_1_wavelets_to_tfrecords.py
@@ -0,0 +1,118 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: Songgx
+@file: 0302_1_wavelets_to_tfrecords.py
+@time: 2016/12/20 19:18
+"""
+
+# https://github.com/tobegit3hub/deep_recommend_system/blob/master/data/convert_cancer_to_tfrecords.py
+import tensorflow as tf
+import os
+import numpy as np
+import math
+
+# The data in cancer.csv:
+# 10,10,10,8,6,1,8,9,1,1
+# 6,2,1,1,1,1,7,1,1,0
+# 2,5,3,3,6,7,7,5,1,1
+
+def get_label(path):
+    switcher = {
+        'blues' : 0,
+        'classical' : 1,
+        'country' : 2,
+        'disco' : 3,
+        'hiphop' : 4,
+        'jazz' : 5,
+        'metal' : 6,
+        'pop' : 7,
+        'reggae' : 8,
+        'rock' : 9,
+    }
+    for key in switcher.keys():
+        if path.find(key) > -1:
+            return switcher.get(key)
+    raise NameError("Path error, can't find respective genre.")
+
+
+def convert_wavelets_to_tfrecords(wavelets_and_spilit_file_folder, spilit_file_path, output_file_path):
+    writer = tf.python_io.TFRecordWriter(output_file_path)
+    cur_cA_path = ""
+    cur_cD_path = ""
+
+    splited_file_array = []
+    for line in open(spilit_file_path, 'r'):
+        splited_file_array.append(line.strip())
+
+    for root, subdirs, files in os.walk(wavelets_and_spilit_file_folder):
+        files.sort()
+        # ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+        # 分别对应[0-9]
+        for file in files:
+            # 判断文件是否在对应的training set 或者 test set 里面，若是，进行转换
+            if file in splited_file_array:
+
+                path = root + "/" + file
+                if path.find("cA") > -1:
+                    cur_cA_path = path
+                elif path.find("cD") > -1:
+                    cur_cD_path = path
+                if cur_cA_path != "" and cur_cA_path[:-1]==cur_cD_path[:-1]:
+                    print("Start to convert {} to {}".format(cur_cA_path[:-1], output_file_path))
+                    cA = np.loadtxt(cur_cA_path, dtype=np.float32, delimiter="\n")
+                    cD = np.loadtxt(cur_cD_path, dtype=np.float32, delimiter="\n")
+                    wavelets = np.vstack((cA, cD))
+
+                    features_cA = []
+                    for i in cA:
+                        val = float(i)
+                        if math.isnan(val):
+                            features_cA.append(0.)
+                        else:
+                            features_cA.append(val)
+
+                    features_cD = []
+                    for i in cD:
+                        val = float(i)
+                        if math.isnan(val):
+                            features_cD.append(0.)
+                        else:
+                            features_cD.append(val)
+
+                    label = get_label(cur_cA_path)
+                    # Write each example one by one
+                    example = tf.train.Example(features=tf.train.Features(feature={
+                        "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),
+                        "features_cA": tf.train.Feature(float_list=tf.train.FloatList(value=features_cA)),
+                        "features_cD": tf.train.Feature(float_list=tf.train.FloatList(value=features_cD)),
+                    }))
+
+                    writer.write(example.SerializeToString())
+                    print("Successfully converted {} to {}".format(cur_cA_path[:-1], output_file_path))
+
+    writer.close()
+    print("###############   Finished.   #############")
+
+# convert_tfrecords("scat_data_test.txt", "scat_data_test.tfrecords", "/merge/")
+
+if __name__ == "__main__":
+
+    wavelets_and_spilit_file_path = 'C:/Users/song/data/wavelets'
+    training_set_path = wavelets_and_spilit_file_path + '/training_set.txt'
+    test_set_path = wavelets_and_spilit_file_path+'/test_set.txt'
+
+    convert_wavelets_to_tfrecords(wavelets_and_spilit_file_path, training_set_path, "merge/wavelet_data_training.tfrecords")
+    convert_wavelets_to_tfrecords(wavelets_and_spilit_file_path, test_set_path, "merge/wavelet_data_test.tfrecords")
+
+    '''
+    # test_get_label()
+
+    for root, subdirs, files in os.walk("C:/Users/song/data/wavelets"):
+        files.sort()
+        # ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+        # 分别对应[0-9]
+        for file in files:
+            path = root + "/" + file
+            print(get_label(path), path)
+    '''
